96617b1f159ab58c061217a52722d4e690f61553
storagerepo <deemsysinchr@gmail.com>
2015-08-12 10:35:01 +0530

    Commit after UI change to Deemsys logo

--- assets/js/common-scripts.js
+++ assets/js/common-scripts.js
@@ -1,4 +1,38 @@
 /*---LEFT BAR ACCORDION----*/
+$(document).on('change', '.btn-file :file', function() {
+  var input = $(this),
+      numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+        
+    });
+});
+function checkForIcon(id){
+  var isChecked = document.getElementById(id).checked;
+  var hideTimeId = id+"_check";
+  var showCheckId = id+"_times";
+  if (isChecked) {
+    document.getElementById(hideTimeId).style.display = "none"; 
+        document.getElementById(showCheckId).style.display = "block"; 
+  }
+  else{
+    document.getElementById(showCheckId).style.display = "none"; 
+     document.getElementById(hideTimeId).style.display = "block"; 
+  }
+}
 $(function() {
     $('#nav-accordion').dcAccordion({
         eventType: 'click',
